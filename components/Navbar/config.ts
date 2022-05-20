c084bae11cf9b1fa6e777d5f68fcb9e89443c356
handou2 <2864466519@qq.com>
2022-05-20 17:16:43 +0800

    fix:数据库连接 新建文章

--- components/Navbar/config.ts
+++ components/Navbar/config.ts
@@ -3,10 +3,10 @@ export const navs = [
     label: "首页",
     value: "/",
   },
-  {
-    label: "咨询",
-    value: "/info",
-  },
+  // {
+  //   label: "咨询",
+  //   value: "/info",
+  // },
   {
     label: "标签",
     value: "/tag",
