1e707bea5302b90bfda34766afc3bae2e72c7f79
handou2 <2864466519@qq.com>
2022-05-23 20:36:42 +0800

    fix:线上数据库连接

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -56,7 +56,7 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
       // 新用户，自动注册
       const user = new User();
       user.nickname = `用户_${Math.floor(Math.random() * 10000)}`;
-      user.avatar = "/public/images/avatar.jpg";
+      user.avatar = "/out/images/avatar.jpg";
       user.job = "暂无";
       user.introduce = "暂无";
 
